919c5974ca2a21facfbe8ede404f062fb865d4a5
Matt Maxwell <mrmaxwell78@gmail.com>
2020-08-07 21:18:31 -0400

    stuff 🦊

--- src/app.js
+++ src/app.js
@@ -2,13 +2,23 @@ if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
         .register("/serviceWorker.js")
-        .then(res => console.log("service worker registered"))
+        // .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
   }
 
 var result = 0;
 var total = 0;
+
+function UpdateBox(event){
+    document.getElementById("numBox").value += event.target.value;
+// console.log(this);
+//console.log(event.target.value)
+}
+
+function Clear(){
+    document.getElementById("numBox").value = 0;
+}
 
 function Add(val){
     total += val;
